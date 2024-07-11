bc71a68444f687592d8050c0e68a95de9640e8d2
Manshi <manshi547075@gmail.com>
2024-07-11 14:05:07 +0530

    Day1

---
+++ Day1/operator.java
@@ -0,0 +1,40 @@
+package Day1;
+
+public class operator {
+    public static void main(String[] args) {
+        //Arithmetic operator
+        int a=2,b=3;
+        a= a+b;//5
+        b=b+a;//8
+        a=a-b;//-3
+        b=b-a;//11
+       
+        System.out.println(a);
+        //2.Comparison operators
+        //[<,>,!,==,<=,=>]
+        //3.Logical operators: &&, || , !
+        //if(!(a>b|| a<10))
+        //4. Assignment operator
+        //x+=3
+        //x-=3
+        //homework: bitwise= &,|,^,>>,<<
+        int sum=0;
+        for(int i=1;i<=25;i++){
+           sum=sum+i;
+           System.out.print(sum+" , ");}
+        int x=5;
+        int c=0;
+        while (x<15) {
+            if(x%2==0){
+                System.out.println(x);
+                x++;}
+            else(x%7==0){
+                System.out.println(x);
+                x++;
+            }
+            
+        }
+         
+        
+    }
+}
---
+++ datatype.java
@@ -0,0 +1,19 @@
+public class datatype {
+    public static void main(String[] args) {
+        /*Primitive datatype
+         * int: 4 bytes
+         * float: decimal upto 6 places:4 bytes
+         * double:decimal upto 15 places:8 bytes
+         * char:2 bytes
+         * long: integer:8 bytes
+         * boolean:1 bit
+         * short:number:2 bytes
+         */
+        int a=1,b=2;
+        a= a+b;
+        System.out.println(a);
+        System.out.print(b);
+
+    }
+    
+}
